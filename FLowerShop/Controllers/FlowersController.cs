b0ef348b71a58b27d6b3c5cce241cc1b22d51bde
lepro83 <72549881+lepro83@users.noreply.github.com>
2023-10-31 18:57:55 +0700

    feat: update seach flower

--- FLowerShop/Controllers/FlowersController.cs
+++ FLowerShop/Controllers/FlowersController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using FLowerShop.Context;
@@ -32,6 +34,35 @@ namespace FLowerShop.Controllers
             detailModel.Flowers = db.FLOWERS.Where(f => f.FLOWERTYPE_ID == flower.FLOWERTYPE_ID && f.FLOWER_ID != flowerId).ToList();
             detailModel.DiscountCodes = db.DISCOUNTCODES.ToList();
             return View(detailModel);
+        }
+
+        public ActionResult Search(string searchQuery, Guid? flowerTypeId)
+        {
+            LoadCommonData();
+            SearchModel searchModel = new SearchModel();
+
+            var flowers = db.FLOWERS.ToList(); // Retrieve all flowers from the database
+
+            // Normalize the search query
+            var normalizedQuery = NormalizeString(searchQuery);
+
+            var filteredFlowers = flowers
+                .Where(f => NormalizeString(f.FLOWER_NAME).Contains(normalizedQuery) &&
+                    (!flowerTypeId.HasValue || f.FLOWERTYPE_ID == flowerTypeId))
+                .ToList();
+
+            var flowerType = db.FLOWERTYPES.ToList();
+            searchModel.Flowers = filteredFlowers;
+            searchModel.FlowersType = flowerType;
+            ViewBag.SearchQuery = searchQuery;
+            ViewBag.SelectedFlowerTypeId = flowerTypeId.ToString();
+
+            return View(searchModel);
+        }
+
+        private string NormalizeString(string input)
+        {
+            return new string(input.Normalize(NormalizationForm.FormD).Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark).ToArray());
         }
     }
 }
---
+++ FLowerShop/Models/SearchModel.cs
@@ -0,0 +1,14 @@
+﻿using FLowerShop.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FLowerShop.Models
+{
+    public class SearchModel
+    {
+        public List<FLOWER> Flowers { get; set; }
+        public List<FLOWERTYPE> FlowersType { get; set; }
+    }
+}
